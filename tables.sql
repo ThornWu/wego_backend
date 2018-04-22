606082132f0613ddb046cdf9e89151c97d5e37e0
Thorn Wu <thornwu@163.com>
2018-04-22 23:49:51 +0800

    update_tables

---
+++ tables.sql
@@ -0,0 +1,53 @@
+CREATE TABLE `user` (
+	`userid`	integer(13),
+	`username`	varchar(30) NOT NULL UNIQUE,
+	`password`	varchar(64) NOT NULL,
+	`email`	varchar(30) NOT NULL,
+	`gender`	boolean NOT NULL,
+	`profile`	varchar(30),
+	`homecity`	varchar(30),
+	PRIMARY KEY(userid)
+);
+
+CREATE TABLE `venue` (
+	`venueid`	varchar(24),
+	`venuename`varchar(50) NOT NULL,
+	`category`	varchar(24) NOT NULL,
+	`latitude`	double NOT NULL,
+	`longitude` double NOT NULL,
+	`address`	varchar(100),
+	`localcity`	varchar(30),
+	PRIMARY KEY(venueid)
+);
+
+CREATE TABLE `tip` (
+	`userid` integer(13) NOT NULL,
+	`venueid` varchar(24) NOT NULL,
+	`createtime` integer(10) NOT NULL,
+	PRIMARY KEY(userid,venueid,createtime)
+);
+
+CREATE TABLE `friendship` (
+	`usera`	integer(13) NOT NULL,
+	`userb` integer(13) NOT NULL,
+	PRIMARY KEY(usera,userb)
+);
+
+CREATE TABLE `category` (
+	`categoryid` varchar(24) primary key,
+	`categoryname` varchar(30) not null,
+	`parentid` varchar(24) not null
+);
+
+CREATE TABLE `favorite` (
+	`userid` integer(13) not null,
+	`venueid` varchar(24) not null,
+	`createtime` integer(10) not null
+);
+
+CREATE TABLE `history` (
+	`history` integer primary key AUTOINCREMENT,
+	`userid` integer(13) not null,
+	`venueid` varchar(24) not null,
+	`createtime` integer(10) not null
+);
